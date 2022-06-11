6d683dc2c30145ff9d7b5e15223aafcb26ed41a6
James <wong.james2003@gmail.com>
2022-06-11 15:18:46 -0400

    add a line of code

--- quote_application.py
+++ quote_application.py
@@ -3,8 +3,10 @@ import json
 
 
 class person:
-    def __init__(self, name, quote):
+    def __init__(self, name, quote, image):
         self.name = name
         self.quote = quote
+        self.image = image
 
 
+person('James Wong', 'hello world', 'no image')
