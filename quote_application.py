2205d48b920d2124c3514afb9595be449d3debb5
James Wong <wong.james2003@gmail.com>
2022-06-11 16:57:20 -0400

    add another class

--- quote_application.py
+++ quote_application.py
@@ -1,4 +1,5 @@
 import json
+from typing_extensions import Self
 
 
 
@@ -9,4 +10,13 @@ class person:
         self.image = image
 
 
-person('James Wong', 'hello world', 'no image')
+class people:
+    def __init__(self, people1, people2, people3):
+        self.people1 = people1
+        self.people2 = people2
+        self.people3 = people3
+
+person1 = person('James', 'python', 'image1')
+person2 = person('Mathew', 'java', 'image2')
+person3 = person('Way', 'Javascript', 'image2')
+
